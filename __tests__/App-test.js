975bd6e3a7b4618dcbb2c0efb85a19f22e216d81
saki <16546851+sakiko-2@users.noreply.github.com>
2020-01-21 02:40:56 +1300

    clean up

--- __tests__/App-test.js
+++ __tests__/App-test.js
@@ -3,7 +3,6 @@ import 'react-vr';
 import React from 'react';
 import App from '../src/App';
 
-// Note: test renderer must be required after react-native.
 import renderer from 'react-test-renderer';
 
 it('renders correctly', () => {
--- __tests__/components/Door-test.js
+++ __tests__/components/Door-test.js
@@ -3,7 +3,6 @@ import 'react-vr';
 import React from 'react';
 import Door from '../../src/components/Door';
 
-// Note: test renderer must be required after react-native.
 import renderer from 'react-test-renderer';
 
 it('renders correctly', () => {
